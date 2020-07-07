928328fd197f63091b3fd0069908dbc4adfb7dfe
Kevin Millan <kmillanr@fractal-software.com>
2020-07-07 18:38:46 -0400

    spark master/worker connection fix

--- streamProcessing/submit.sh
+++ streamProcessing/submit.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-
-export SPARK_MASTER_URL=spark://${SPARK_MASTER_NAME}:${SPARK_MASTER_PORT}
-export SPARK_HOME=/spark
-
 /spark/bin/spark-submit \
-    --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.5,com.johnsnowlabs.nlp:spark-nlp_2.11:2.4.5 \
+    --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.5.3,com.johnsnowlabs.nlp:spark-nlp_2.11:2.5.3 \
     --class com.inndata.StructuredStreaming.Kafka \
-    --master spark://spark-master:7077 /kafka-pyspark-streaming.py
+    --master spark://master:7077 /kafka-pyspark-streaming.py
